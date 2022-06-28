d0790ce22a3de98ed346398dda7e1600da226abf
reus <reusee@gmail.com>
2022-06-28 20:48:11 +0800

    minor clean-ups

--- cmd/db-server/version.go
+++ cmd/db-server/version.go
@@ -24,18 +24,16 @@ func (_ Def) Version() (
 ) {
 
 	var p Parser
-	parsers = ArgumentParsers{
-		p.MatchStr("--version", p.End(func() {
-			// if the argument passed in is "--version", return version info and exit
-			fmt.Println("MatrixOne build info:")
-			fmt.Printf("  The golang version used to build this binary: %s\n", GoVersion)
-			fmt.Printf("  Git branch name: %s\n", BranchName)
-			fmt.Printf("  Last git commit ID: %s\n", LastCommitId)
-			fmt.Printf("  Buildtime: %s\n", BuildTime)
-			fmt.Printf("  Current Matrixone version: %s\n", MoVersion)
-			os.Exit(0)
-		})),
-	}
+	parsers = append(parsers, p.MatchStr("--version", p.End(func() {
+		// if the argument passed in is "--version", return version info and exit
+		fmt.Println("MatrixOne build info:")
+		fmt.Printf("  The golang version used to build this binary: %s\n", GoVersion)
+		fmt.Printf("  Git branch name: %s\n", BranchName)
+		fmt.Printf("  Last git commit ID: %s\n", LastCommitId)
+		fmt.Printf("  Buildtime: %s\n", BuildTime)
+		fmt.Printf("  Current Matrixone version: %s\n", MoVersion)
+		os.Exit(0)
+	})))
 
 	return
 }
