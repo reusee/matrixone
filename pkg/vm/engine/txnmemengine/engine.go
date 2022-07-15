22d1ddd1325147263fe8b6f88e5a151df4ec7565
reus <reusee@gmail.com>
2022-07-15 16:58:27 +0800

    more txnmemengine codes

--- pkg/vm/engine/txnmemengine/engine.go
+++ pkg/vm/engine/txnmemengine/engine.go
@@ -15,41 +15,152 @@
 package engine
 
 import (
+	"bytes"
+	"context"
+	"encoding/gob"
+	"sync"
+
+	"github.com/matrixorigin/matrixone/pkg/logservice"
+	logservicepb "github.com/matrixorigin/matrixone/pkg/pb/logservice"
+	"github.com/matrixorigin/matrixone/pkg/pb/metadata"
+	"github.com/matrixorigin/matrixone/pkg/pb/txn"
 	"github.com/matrixorigin/matrixone/pkg/txn/client"
 	"github.com/matrixorigin/matrixone/pkg/vm/engine"
 )
 
 // Engine is an engine.Engine impl
 type Engine struct {
+
+	// hakeeper
+	hakeeperClient logservice.CNHAKeeperClient
+	clusterDetails struct {
+		sync.Mutex
+		logservicepb.ClusterDetails
+	}
+
+	fatal func()
 }
 
-func New() *Engine {
-	return &Engine{}
+func New(
+	ctx context.Context,
+	hakeeperClient logservice.CNHAKeeperClient,
+) *Engine {
+
+	ctx, cancel := context.WithCancel(ctx)
+	fatal := func() {
+		cancel()
+	}
+
+	engine := &Engine{
+		hakeeperClient: hakeeperClient,
+		fatal:          fatal,
+	}
+	go engine.startHAKeeperLoop(ctx)
+
+	return engine
 }
 
 var _ engine.Engine = new(Engine)
 
-func (*Engine) Create(dbName string, txn client.TxnOperator) error {
-	//TODO
+func (e *Engine) Create(dbName string, txnOperator client.TxnOperator) error {
+
+	// for ddl operations, broadcast to all DNs
+	var requests []txn.TxnRequest
+	for _, node := range e.getDataNodes() {
+		requests = append(requests, txn.TxnRequest{
+			Method: txn.TxnMethod_Write,
+			CNRequest: &txn.CNOpRequest{
+				OpCode: opCreateDatabase,
+				Payload: mustEncodePayload(createDatabasePayload{
+					Name: dbName,
+				}),
+				Target: metadata.DNShard{
+					Address: node.ServiceAddress,
+				},
+			},
+		})
+	}
+
+	ctx := context.TODO()
+	result, err := txnOperator.WriteAndCommit(ctx, requests)
+	if err != nil {
+		return err
+	}
+	if err := errorFromTxnResponses(result.Responses); err != nil {
+		return err
+	}
+
 	return nil
 }
 
-func (*Engine) Database(dbName string, txn client.TxnOperator) (engine.Database, error) {
-	//TODO
+func (e *Engine) Database(dbName string, txnOperator client.TxnOperator) (engine.Database, error) {
+
+	ctx := context.TODO()
+	result, err := txnOperator.Read(ctx, []txn.TxnRequest{
+		{
+			Method: txn.TxnMethod_Read,
+			CNRequest: &txn.CNOpRequest{
+				OpCode: opOpenDatabase,
+				Payload: mustEncodePayload(openDatabasePayload{
+					Name: dbName,
+				}),
+				Target: metadata.DNShard{
+					// use first DN
+					Address: e.getDataNodes()[0].ServiceAddress,
+				},
+			},
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+	if err := errorFromTxnResponses(result.Responses); err != nil {
+		return nil, err
+	}
+
 	return nil, nil
 }
 
-func (*Engine) Databases(txn client.TxnOperator) []string {
+func (e *Engine) Databases(txnOperator client.TxnOperator) ([]string, error) {
+
+	ctx := context.TODO()
+	result, err := txnOperator.Read(ctx, []txn.TxnRequest{
+		{
+			Method: txn.TxnMethod_Read,
+			CNRequest: &txn.CNOpRequest{
+				OpCode: opGetDatabases,
+				Target: metadata.DNShard{
+					// use first DN
+					Address: e.getDataNodes()[0].ServiceAddress,
+				},
+			},
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+	if err := errorFromTxnResponses(result.Responses); err != nil {
+		return nil, err
+	}
+
+	var dbNames []string
+	for _, resp := range result.Responses {
+		var payload getDatabasesPayload
+		if err := gob.NewDecoder(bytes.NewReader(resp.CNOpResponse.Payload)).Decode(&payload); err != nil {
+			return nil, err
+		}
+		dbNames = append(dbNames, payload.Names...)
+	}
+
+	return dbNames, nil
+}
+
+func (*Engine) Delete(dbName string, txnOperator client.TxnOperator) error {
 	//TODO
 	return nil
 }
 
-func (*Engine) Delete(dbName string, txn client.TxnOperator) error {
-	//TODO
-	return nil
-}
-
-func (*Engine) Nodes(txn client.TxnOperator) engine.Nodes {
+func (*Engine) Nodes(txnOperator client.TxnOperator) engine.Nodes {
 	//TODO
 	return nil
 }
