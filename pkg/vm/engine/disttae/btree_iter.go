d354aea44865ea69663ccc804801cdce0b656c99
reus <reusee@gmail.com>
2023-03-25 22:56:11 +0800

    disttae: add google/btree iter

---
+++ pkg/vm/engine/disttae/btree_iter.go
@@ -0,0 +1,83 @@
+// Copyright 2023 Matrix Origin
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package disttae
+
+import "github.com/google/btree"
+
+type btreeIter[T any] struct {
+	tree     *btree.BTreeG[T]
+	entries  []T
+	i        int
+	pivot    *T
+	lessFunc btree.LessFunc[T]
+}
+
+const maxBTreeCachedEntries = 256
+
+func newBTreeIter[T any](tree *btree.BTreeG[T], lessFunc btree.LessFunc[T]) *btreeIter[T] {
+	return &btreeIter[T]{
+		tree:     tree.Clone(),
+		entries:  make([]T, 0, maxBTreeCachedEntries),
+		lessFunc: lessFunc,
+	}
+}
+
+func (b *btreeIter[T]) Next() bool {
+	if b.i >= len(b.entries) {
+		// load
+		b.i = 0
+		b.entries = b.entries[:0]
+
+		if b.pivot == nil {
+			// from start
+			b.tree.Ascend(func(entry T) bool {
+				b.entries = append(b.entries, entry)
+				if len(b.entries) >= maxBTreeCachedEntries {
+					return false
+				}
+				return true
+			})
+
+		} else {
+			// from pivot
+			b.tree.AscendGreaterOrEqual(*b.pivot, func(entry T) bool {
+				if !b.lessFunc(entry, *b.pivot) &&
+					!b.lessFunc(*b.pivot, entry) {
+					// equal to pivot, skip
+					return true
+				}
+				b.entries = append(b.entries, entry)
+				if len(b.entries) >= maxBTreeCachedEntries {
+					return false
+				}
+				return true
+			})
+		}
+
+		if len(b.entries) > 0 {
+			lastEntry := b.entries[len(b.entries)-1]
+			b.pivot = &lastEntry
+		}
+
+	} else {
+		b.i++
+	}
+
+	return b.i < len(b.entries)
+}
+
+func (b *btreeIter[T]) Entry() T {
+	return b.entries[b.i]
+}
---
+++ pkg/vm/engine/disttae/btree_iter_test.go
@@ -0,0 +1,56 @@
+// Copyright 2023 Matrix Origin
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package disttae
+
+import (
+	"math/rand"
+	"testing"
+
+	"github.com/google/btree"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBTreeIter(t *testing.T) {
+	less := func(a, b int) bool {
+		return a < b
+	}
+	tree := btree.NewG(2, less)
+	for _, i := range rand.Perm(1024) {
+		tree.ReplaceOrInsert(i)
+	}
+	iter := newBTreeIter(tree, less)
+	last := -1
+	for iter.Next() {
+		entry := iter.Entry()
+		assert.Equal(t, last+1, entry)
+		last = entry
+	}
+}
+
+func BenchmarkBTreeIter(b *testing.B) {
+	less := func(a, b int) bool {
+		return a < b
+	}
+	tree := btree.NewG(2, less)
+	for _, i := range rand.Perm(1024) {
+		tree.ReplaceOrInsert(i)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		iter := newBTreeIter(tree, less)
+		for iter.Next() {
+		}
+	}
+}
