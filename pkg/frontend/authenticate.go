462325c6a242660b02e2600068f86c7b6a75cdd4
daviszhen <daviszhen007@gmail.com>
2023-04-28 21:29:39 +0800

    fix context

--- pkg/frontend/authenticate.go
+++ pkg/frontend/authenticate.go
@@ -6404,7 +6404,7 @@ func InitSysTenant(ctx context.Context, aicm *defines.AutoIncrCacheManager) erro
 		return err
 	}
 
-	bh.Exec(ctx, createMoIndexesSql)
+	err = bh.Exec(ctx, createMoIndexesSql)
 	if err != nil {
 		return err
 	}
