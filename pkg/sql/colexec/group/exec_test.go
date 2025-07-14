8451cdb82bdeb380614cc023dc5c5208cdb5409d
reus <reusee@gmail.com>
2025-07-14 14:41:06 +0800

    colexec/group: more tests

--- pkg/sql/colexec/group/exec_test.go
+++ pkg/sql/colexec/group/exec_test.go
@@ -15,6 +15,8 @@
 package group
 
 import (
+	"testing"
+
 	"github.com/matrixorigin/matrixone/pkg/common/mpool"
 	"github.com/matrixorigin/matrixone/pkg/container/batch"
 	"github.com/matrixorigin/matrixone/pkg/container/types"
@@ -25,7 +27,6 @@ import (
 	"github.com/matrixorigin/matrixone/pkg/testutil"
 	"github.com/matrixorigin/matrixone/pkg/vm"
 	"github.com/stretchr/testify/require"
-	"testing"
 )
 
 // hackAggExecToTest 是一个不带任何逻辑的AggExec,主要用于单测中检查各种接口的调用次数。
